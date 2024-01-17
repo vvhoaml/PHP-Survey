79bd74ef024f5c50a6bf0f1ec3d74edb2929b436
whoami <lallipapi4@yandex.ru>
2024-01-17 15:03:26 +1000

    Удалось реализовать нужный функционал. Параметр id, взятый из url сверяется с таблицей USERS, и если такой id существует, то отзыв записывается в таблицу SURVEYS. Но до принципов solid в моем коде еще далеко. Так же присутсвует небольшой баг. Если id не задан или не существует в таблице USERS, то ошибка об этом выходит прямо в форме. Надо пофиксить

--- index.php
+++ index.php
@@ -3,12 +3,50 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<title>Оставьте отзыв</title>
+	<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$user_id = $_POST['id'];
+	$mark = $_POST['mark'];
+	$text = $_POST['text'];
+
+	require_once 'db.php';
+	$db = new DB();
+	if ($db->insertData($user_id, $mark, $text)) {
+		echo '<script>alert("Данные успешно сохранены");</script>';
+		die;
+	} else {
+		echo "Произошла ошибка. Пожалуйста, повторите попытку.";
+	}
+}
+?>
 </head>
 <body>
-	<?php
-	echo 123;
-	?>
+<h2>Оставьте отзыв</h2>
+	<form action="index.php" method="post">
+
+	<input type="hidden" name="id" value="<?php echo $_GET['id']; ?>">
+
+	<input type="radio" id="mark5" name="mark" value="5" />
+  	<label for="mark5">5</label><br>
+
+  	<input type="radio" id="mark4" name="mark" value="4" />
+  	<label for="mark4">4</label><br>
+
+ 	<input type="radio" id="mark3" name="mark" value="3" />
+  	<label for="mark3">3</label><br>
+
+  	<input type="radio" id="mark2" name="mark" value="2" />
+  	<label for="mark2">2</label><br>
+
+ 	<input type="radio" id="mark1" name="mark" value="1" />
+ 	<label for="mark1">1</label><br>
+
+	<label for="text">Оставьте комментарий к отзыву:</label><br>
+	<textarea id="text" name="text" rows="4" cols="50"></textarea><br>
+	
+	<input type="submit" value="Отправить">
+	</form>
 
 </body>
 </html>
